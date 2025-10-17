4becb6a9856eb09ba83f05d49615c92940f50903
rodrigo ramos <rodrigo.ramos.santos@gmail.com>
2025-10-17 15:34:50 -0300

    feat: htop

--- scripts/install-apps/install_dev_base.sh
+++ scripts/install-apps/install_dev_base.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source "${SCRIPT_DIR}/shared_functions.sh"
 
 # base apps
-sudo apt update && sudo apt install curl flatpak fzf git stow tree zsh -y
+sudo apt update && sudo apt install curl flatpak fzf git stow tree zsh htop -y
 
 # zsh
 configure_zsh
